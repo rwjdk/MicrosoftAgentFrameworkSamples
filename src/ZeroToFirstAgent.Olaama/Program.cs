812801c26a9ac20e0420332257f4efe5f4429154
Rasmus Wulff Jensen <rasmus.wulff.jensen@gmail.com>
2025-10-02 18:34:36 +0200

    All Zero to first Agent

---
+++ src/ZeroToFirstAgent.Olaama/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OllamaSharp;
+
+IChatClient client = new OllamaApiClient("http://localhost:11434", "llama3.2:1b");
+AIAgent agent = new ChatClientAgent(client);
+AgentRunResponse response = await agent.RunAsync("What is the Capital of Sweden?");
+Console.WriteLine(response);
