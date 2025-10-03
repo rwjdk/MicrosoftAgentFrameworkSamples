cea05f67e0fe0fe3923659091e5ba06bdb1f4e05
Rasmus Wulff Jensen <rasmus.wulff.jensen@gmail.com>
2025-10-03 19:22:01 +0200

    Added Conversations Sample

---
+++ src/ConversationThreads/Program.cs
@@ -0,0 +1,48 @@
+﻿using Azure.AI.OpenAI;
+using Microsoft.Agents.AI;
+using OpenAI;
+using Shared;
+using System.ClientModel;
+using ConversationThreads;
+using Microsoft.Extensions.AI;
+using ChatMessage = Microsoft.Extensions.AI.ChatMessage;
+
+Configuration configuration = ConfigurationManager.GetConfiguration();
+
+AzureOpenAIClient client = new AzureOpenAIClient(new Uri(configuration.Endpoint), new ApiKeyCredential(configuration.Key));
+
+AIAgent agent = client
+    .GetChatClient(configuration.ChatDeploymentName)
+    .CreateAIAgent(instructions: "You are a Friendly AI Bot, answering questions");
+
+AgentThread thread = agent.GetNewThread();
+
+const bool optionToResume = true; //Set this to true to test resume of previous conversations
+
+if (optionToResume)
+{
+    thread = await AgentThreadPersistence.ResumeChatIfRequestedAsync(agent);
+}
+
+while (true)
+{
+    Console.Write("> ");
+    string? input = Console.ReadLine();
+    if (!string.IsNullOrWhiteSpace(input))
+    {
+        ChatMessage message = new ChatMessage(ChatRole.User, input);
+        await foreach (AgentRunResponseUpdate update in agent.RunStreamingAsync(message, thread))
+        {
+            Console.Write(update);
+        }
+    }
+
+    Console.WriteLine();
+    Console.WriteLine(string.Empty.PadLeft(50, '*'));
+    Console.WriteLine();
+
+    if (optionToResume)
+    {
+        await AgentThreadPersistence.StoreThreadAsync(thread);
+    }
+}
