c9515d64f2a5948b42cf9ce6b5057ae8a2973143
Rasmus Wulff Jensen <rasmus.wulff.jensen@gmail.com>
2025-10-10 04:50:32 +0200

    Added OpenRouter Sample

---
+++ src/ZeroToFirstAgent.OpenRouter/Program.cs
@@ -0,0 +1,32 @@
+﻿//YouTube video that cover this sample: https://youtu.be/GbyEQWwBMFk
+
+/* Steps:
+ * 1: Get a OpenRouter API Key (https://openrouter.ai/settings/keys)
+ * 2: Add Nuget Packages (OpenAI + Microsoft.Agents.AI.OpenAI)
+ * 3: Create an OpenAIClient
+ * 4: Get a ChatClient and Create an AI Agent from it
+ * 5: Call RunAsync or RunStreamingAsync
+ */
+
+using System.ClientModel;
+using Microsoft.Agents.AI;
+using OpenAI;
+
+const string apiKey = "<YourApiKey>";
+const string model = "<yourModelName>";
+
+OpenAI.OpenAIClient client = new OpenAIClient(new ApiKeyCredential(apiKey), new OpenAIClientOptions
+{
+    Endpoint = new Uri("https://openrouter.ai/api/v1")
+});
+AIAgent agent = client.GetChatClient(model).CreateAIAgent();
+
+AgentRunResponse response = await agent.RunAsync("What is the Capital of Germany?");
+Console.WriteLine(response);
+
+Console.WriteLine("---");
+
+await foreach (AgentRunResponseUpdate update in agent.RunStreamingAsync("How to make soup?"))
+{
+    Console.Write(update);
+}
