3421fb411f4e8865af176132707c98115ae4514a
Rasmus Wulff Jensen <rasmus.wulff.jensen@gmail.com>
2025-11-05 15:43:49 +0100

    Still working

--- src/Agent2Agent.Client/Program.cs
+++ src/Agent2Agent.Client/Program.cs
@@ -1,12 +1,16 @@
 ﻿// Create root command with options
 
+using A2A;
+using Azure.AI.OpenAI;
 using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
 using Microsoft.Extensions.Logging;
+using Shared;
+using System.ClientModel;
 using System.CommandLine;
-using Agent2Agent.Client;
+using OpenAI;
 
 await Task.Delay(5000);
-
 RootCommand rootCommand = new RootCommand("A2AClient");
 rootCommand.SetAction((_, token) => HandleCommandsAsync(token));
 
@@ -15,6 +19,7 @@ return await rootCommand.Parse(args).InvokeAsync();
 
 static async Task HandleCommandsAsync(CancellationToken cancellationToken)
 {
+    Configuration configuration = Shared.ConfigurationManager.GetConfiguration();
     // Set up the logging
     using var loggerFactory = LoggerFactory.Create(builder =>
     {
@@ -23,12 +28,17 @@ static async Task HandleCommandsAsync(CancellationToken cancellationToken)
     });
     var logger = loggerFactory.CreateLogger("A2AClient");
 
-    var agentUrls = "http://localhost:5000/";
+    var agentUrl = "http://localhost:5000/";
 
-    // Create the Host agent
-    var hostAgent = new HostClientAgent(loggerFactory);
-    await hostAgent.InitializeAgentAsync(agentUrls!.Split(";"));
-    AgentThread thread = hostAgent.Agent!.GetNewThread();
+    AIAgent remoteAgent = await CreateRemoteAgentAsync(agentUrl);
+    List<AITool> tools = [remoteAgent.AsAIFunction()];
+
+    // Create the agent that uses the remote agents as tools
+    ChatClientAgent clientAgent = new AzureOpenAIClient(new Uri(configuration.AzureOpenAiEndpoint), new ApiKeyCredential(configuration.AzureOpenAiKey))
+        .GetChatClient("gpt-4.1")
+        .CreateAIAgent(instructions: "You specialize in handling queries for users and using your tools to provide answers.", name: "HostClient", tools: tools);
+
+    AgentThread thread = clientAgent.GetNewThread();
     try
     {
         while (true)
@@ -47,7 +57,7 @@ static async Task HandleCommandsAsync(CancellationToken cancellationToken)
                 break;
             }
 
-            var agentResponse = await hostAgent.Agent!.RunAsync(message, thread, cancellationToken: cancellationToken);
+            var agentResponse = await clientAgent.RunAsync(message, thread, cancellationToken: cancellationToken);
             foreach (var chatMessage in agentResponse.Messages)
             {
                 Console.ForegroundColor = ConsoleColor.Cyan;
@@ -60,5 +70,26 @@ static async Task HandleCommandsAsync(CancellationToken cancellationToken)
     {
         logger.LogError(ex, "An error occurred while running the A2AClient");
         return;
+    }
+}
+
+static async Task<AIAgent> CreateRemoteAgentAsync(string agentUri)
+{
+    Uri url = new Uri(agentUri);
+    HttpClient httpClient = new()
+    {
+        Timeout = TimeSpan.FromSeconds(60)
+    };
+
+    A2ACardResolver agentCardResolver = new A2ACardResolver(url, httpClient);
+
+    try
+    {
+        return await agentCardResolver.GetAIAgentAsync();
+    }
+    catch (Exception e)
+    {
+        Utils.WriteLineRed(e);
+        throw;
     }
 }
