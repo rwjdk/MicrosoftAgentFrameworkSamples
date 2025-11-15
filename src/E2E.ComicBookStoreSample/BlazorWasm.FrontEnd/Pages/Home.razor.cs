98655cae4a56c78be15e9ce67f37084861fdff15
Rasmus Wulff Jensen <rasmus.wulff.jensen@gmail.com>
2025-11-15 10:51:36 +0100

    WIP

---
+++ src/E2E.ComicBookStoreSample/BlazorWasm.FrontEnd/Pages/Home.razor.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using Microsoft.Agents.AI;
+
+namespace BlazorWasm.FrontEnd.Pages;
+
+[UsedImplicitly]
+public partial class Home([FromKeyedServices("comic-book-guy-agent")] ChatClientAgent comicBookAgent)
+{
+    private string? _question;
+    private string? _answer;
+
+    private async Task AskAi()
+    {
+        if (string.IsNullOrWhiteSpace(_question))
+        {
+            return;
+        }
+
+        AgentRunResponse response = await comicBookAgent.RunAsync(_question);
+        _answer = response.Text;
+        StateHasChanged();
+    }
+}
