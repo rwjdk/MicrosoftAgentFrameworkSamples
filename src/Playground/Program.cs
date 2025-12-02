0dadef2d6a89b1edbec710238a9c5c585deb5de7
Rasmus Wulff Jensen <rasmus.wulff.jensen@gmail.com>
2025-12-02 18:08:55 +0100

    Speech Generation Sample

--- src/Playground/Program.cs
+++ src/Playground/Program.cs
@@ -4,63 +4,66 @@
 //- Microsoft.Agents.AI.Hosting.AgentCatalog TODO: Guess this is something to be used in AI Foundry
 
 #pragma warning disable OPENAI001
+using A2A;
 using Azure.AI.OpenAI;
+using CSCore;
+using CSCore.Codecs;
+using CSCore.SoundOut;
+using GenerativeAI;
+using GenerativeAI.Microsoft;
 using Microsoft.Agents.AI;
 using Microsoft.Extensions.AI;
 using OpenAI;
+using OpenAI.Audio;
 using OpenAI.Responses;
 using Shared;
 using System.ClientModel;
 using System.ClientModel.Primitives;
+using System.IO;
 using System.Text;
 using System.Text.Json;
-using A2A;
-using GenerativeAI;
-using GenerativeAI.Microsoft;
-
+using CSCore;
+using CSCore.Codecs;
+using CSCore.Codecs.MP3;
+using CSCore.SoundOut;
+using CSCore.Codecs;
 
 Console.WriteLine("");
 Console.Clear();
 
 Configuration configuration = ConfigurationManager.GetConfiguration();
 
-string apiKey = configuration.GoogleGeminiApiKey;
-const string model = GoogleAIModels.Gemini25Pro;
+OpenAIClient openAiClient = new OpenAIClient(configuration.OpenAiApiKey);
+AzureOpenAIClient azureOpenAIClient = new AzureOpenAIClient(new Uri(configuration.AzureOpenAiEndpoint), new ApiKeyCredential(configuration.AzureOpenAiKey));
 
-IChatClient clientG = new GenerativeAIChatClient(apiKey, model);
+/* Pricing (as of 1st of December 2025)
+ * - gpt-4o-mini-tts    $0.015 / minute
+ * - tts:               15 USD / 1 Million Chars
+ */
 
-await foreach (ChatResponseUpdate update in clientG.GetStreamingResponseAsync("What is the weather like in Paris", new ChatOptions
-               {
-                   Tools = [AIFunctionFactory.Create(GetWeather)]
-               }))
+AudioClient audioClient = openAiClient.GetAudioClient("gpt-4o-mini-tts");
+GeneratedSpeechVoice voice = new GeneratedSpeechVoice("nova"); //nova, shimmer, echo, onyx, fable, alloy'.
+string text = "Hi! Welcome to this video about OpenAI's AudioClient. I'm an AI speaking the words Rasmus entered in his program";
+ClientResult<BinaryData> result = audioClient.GenerateSpeech(text, voice, new SpeechGenerationOptions
 {
-    Console.WriteLine(update);
-}
-
-
-ChatClientAgent agentF = new(clientG, tools: [AIFunctionFactory.Create(GetWeather)]);
-
-AgentRunResponse agentRunResponse = await agentF.RunAsync("What is the weather like in Paris");
-Console.WriteLine(agentRunResponse);
-
-await foreach (AgentRunResponseUpdate update in agentF.RunStreamingAsync("What is the weather like in Paris"))
-{
-    Console.Write(update);
-}
-
-
-AzureOpenAIClient client = new(new Uri(configuration.AzureOpenAiEndpoint), new ApiKeyCredential(configuration.AzureOpenAiKey), new AzureOpenAIClientOptions
-{
-    //Transport = new HttpClientPipelineTransport(httpClient)
+    Instructions = "Speak like a little old lady", //Does not do anything with 'tts', and on 'gpt-4o-mini-tts' it have effect, but nothing special
+    ResponseFormat = new GeneratedSpeechFormat("mp3"), //mp3, opus, aac, flac, wav and pcm
+    //SpeedRatio = 1 //Speed of the voice
 });
 
+byte[] buffer = result.Value.ToArray();
+File.WriteAllBytes("X:\\test.mp3", buffer);
 
-AIAgent agent = client
-    .GetOpenAIResponseClient("gpt-5-mini")
-    .CreateAIAgent();
+byte[] bytes = result.Value.ToArray();
+MemoryStream memoryStream = new(bytes);
 
-AgentRunResponse response = await agent.RunAsync("What is the capital of France and how many live there?");
-Console.WriteLine(response);
+IWaveSource waveSource = new Mp3MediafoundationDecoder(memoryStream);
+ISoundOut soundOut = new WasapiOut();
+soundOut.Initialize(waveSource);
+soundOut.Play();
+
+
+Console.WriteLine();
 
 //await AzureOpenAiFoundry.Run(configuration);
 //await FileTool.Run(configuration);
