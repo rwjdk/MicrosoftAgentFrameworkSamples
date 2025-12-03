36404000175a413b9306f9b49db411424d99f5db
Rasmus Wulff Jensen <rasmus.wulff.jensen@gmail.com>
2025-12-03 19:38:18 +0100

    Tool Call Service Injection Sample

--- src/Playground/Program.cs
+++ src/Playground/Program.cs
@@ -7,26 +7,28 @@
 using A2A;
 using Azure.AI.OpenAI;
 using CSCore;
+using CSCore;
 using CSCore.Codecs;
+using CSCore.Codecs;
+using CSCore.Codecs;
+using CSCore.Codecs.MP3;
+using CSCore.SoundOut;
 using CSCore.SoundOut;
 using GenerativeAI;
 using GenerativeAI.Microsoft;
 using Microsoft.Agents.AI;
 using Microsoft.Extensions.AI;
+using Microsoft.Extensions.DependencyInjection;
 using OpenAI;
 using OpenAI.Audio;
 using OpenAI.Responses;
+using Playground;
 using Shared;
 using System.ClientModel;
 using System.ClientModel.Primitives;
 using System.IO;
 using System.Text;
 using System.Text.Json;
-using CSCore;
-using CSCore.Codecs;
-using CSCore.Codecs.MP3;
-using CSCore.SoundOut;
-using CSCore.Codecs;
 
 Console.WriteLine("");
 Console.Clear();
@@ -36,34 +38,19 @@ Configuration configuration = ConfigurationManager.GetConfiguration();
 OpenAIClient openAiClient = new OpenAIClient(configuration.OpenAiApiKey);
 AzureOpenAIClient azureOpenAIClient = new AzureOpenAIClient(new Uri(configuration.AzureOpenAiEndpoint), new ApiKeyCredential(configuration.AzureOpenAiKey));
 
-/* Pricing (as of 1st of December 2025)
- * - gpt-4o-mini-tts    $0.015 / minute
- * - tts:               15 USD / 1 Million Chars
- */
+ServiceCollection services = new();
+services.AddScoped<HttpClient>();
+services.AddScoped<ToolWithDiNeed>();
+IServiceProvider serviceProvider = services.BuildServiceProvider();
 
-AudioClient audioClient = openAiClient.GetAudioClient("gpt-4o-mini-tts");
-GeneratedSpeechVoice voice = new GeneratedSpeechVoice("nova"); //nova, shimmer, echo, onyx, fable, alloy'.
-string text = "Hi! Welcome to this video about OpenAI's AudioClient. I'm an AI speaking the words Rasmus entered in his program";
-ClientResult<BinaryData> result = audioClient.GenerateSpeech(text, voice, new SpeechGenerationOptions
-{
-    Instructions = "Speak like a little old lady", //Does not do anything with 'tts', and on 'gpt-4o-mini-tts' it have effect, but nothing special
-    ResponseFormat = new GeneratedSpeechFormat("mp3"), //mp3, opus, aac, flac, wav and pcm
-    //SpeedRatio = 1 //Speed of the voice
-});
+ChatClientAgent agent = azureOpenAIClient
+    .GetChatClient("gpt-4.1")
+    .CreateAIAgent(
+        tools: [AIFunctionFactory.Create(serviceProvider.GetRequiredService<ToolWithDiNeed>().GetNews)],
+        services: serviceProvider);
 
-byte[] buffer = result.Value.ToArray();
-File.WriteAllBytes("X:\\test.mp3", buffer);
-
-byte[] bytes = result.Value.ToArray();
-MemoryStream memoryStream = new(bytes);
-
-IWaveSource waveSource = new Mp3MediafoundationDecoder(memoryStream);
-ISoundOut soundOut = new WasapiOut();
-soundOut.Initialize(waveSource);
-soundOut.Play();
-
-
-Console.WriteLine();
+AgentRunResponse response = await agent.RunAsync("Get me the news");
+Console.WriteLine(response);
 
 //await AzureOpenAiFoundry.Run(configuration);
 //await FileTool.Run(configuration);
