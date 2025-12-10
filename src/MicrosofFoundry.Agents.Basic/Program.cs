703635e7cf66d89e4ebc32ef85d2e38bf56d7ca2
Rasmus Wulff Jensen <rasmus.wulff.jensen@gmail.com>
2025-12-10 06:04:17 +0100

    Foundry Agent Sample

---
+++ src/MicrosofFoundry.Agents.Basic/Program.cs
@@ -0,0 +1,110 @@
+﻿using Azure.AI.Projects;
+using Azure.AI.Projects.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Shared;
+using System.ClientModel;
+using OpenAI.Responses;
+
+#pragma warning disable OPENAI001
+
+Console.Clear();
+
+Configuration configuration = ConfigurationManager.GetConfiguration();
+
+AIProjectClient client = new AIProjectClient(new Uri(configuration.AzureAiFoundryAgentEndpoint), new AzureCliCredential());
+
+string modelDeploymentName = "gpt-4.1-mini";
+string myAgentName = "myAgent";
+string myInstructions = "You are a nice AI";
+
+//Step 0 (Optional): Ensure Model for Agent is Deployed
+try
+{
+    await client.Deployments.GetDeploymentAsync(modelDeploymentName);
+}
+catch (ClientResultException e)
+{
+    if (e.Status == 404)
+    {
+        Console.WriteLine($"Model Deployment '{modelDeploymentName}' was not found");
+        return;
+    }
+    else
+    {
+        throw;
+    }
+}
+
+//Step 1: Create/Update Agent if it does not exist
+try
+{
+    ClientResult<AgentRecord> clientResult = await client.Agents.GetAgentAsync(agentName: myAgentName);
+
+    //Let's ensure Agent is as we have defined by making a new version (if definition is the same nothing will happen)
+    //await CreateAgent(myInstructions);
+}
+catch (ClientResultException e)
+{
+    if (e.Status == 404)
+    {
+        Console.WriteLine("Agent not found: Creating it");
+        await CreateAgent(myInstructions);
+    }
+    else
+    {
+        throw;
+    }
+}
+
+ChatClientAgent agentByName = client.GetAIAgent(name: myAgentName);
+
+AgentRunResponse response = await agentByName.RunAsync("Hi there");
+Console.WriteLine(response);
+
+response = await agentByName.RunAsync("What is 23434343*3434343/2323232 (use tools to calculate)");
+Console.WriteLine(response);
+
+response = await agentByName.RunAsync("What is the biggest news story today?");
+Console.WriteLine(response);
+
+//Let's make a V2 with new instructions
+await CreateAgent("Speak like a pirate");
+
+ChatClientAgent agentV2 = client.GetAIAgent(name: myAgentName);
+response = await agentV2.RunAsync("Hi there");
+Console.WriteLine(response);
+
+AgentVersion agentV1 = (await client.Agents.GetAgentVersionAsync(myAgentName, "1")).Value;
+ChatClientAgent agentByVersion = client.GetAIAgent(agentV1);
+
+response = await agentByVersion.RunAsync("Hi Agent 1");
+Console.WriteLine(response);
+
+return;
+
+async Task CreateAgent(string instructions)
+{
+    await client.Agents.CreateAgentVersionAsync(
+        agentName: myAgentName,
+        options: new AgentVersionCreationOptions(
+            new PromptAgentDefinition(modelDeploymentName)
+            {
+                Tools =
+                {
+                    new CodeInterpreterTool(new CodeInterpreterToolContainer(new AutomaticCodeInterpreterToolContainerConfiguration())),
+                    new WebSearchTool(),
+                    //MCP Tools can be defined by can't be properly consumed by MS Agent Framework :-(
+                    //new McpTool("TrelloDotNetToolAssistant", new Uri("https://trellodotnetassistantbackend.azurewebsites.net/runtime/webhooks/mcp?code=Tools")),
+                },
+                Instructions = instructions,
+
+                //NB: Reasoning Effort is buggy at the moment in the portal :-(
+                //ReasoningOptions = new ResponseReasoningOptions
+                //{
+                //    ReasoningEffortLevel = ResponseReasoningEffortLevel.Low
+                //}
+            }
+        )
+    );
+}
