cbe0e75c508c5d16acc99effafb84784d6e46d5f
Rasmus Wulff Jensen <rasmus.wulff.jensen@gmail.com>
2026-04-11 17:52:30 +0200

    wip

--- src/ZeroToFirstAgent.FoundryLocal/Program.cs
+++ src/ZeroToFirstAgent.FoundryLocal/Program.cs
@@ -1,22 +1,21 @@
 ﻿using System.ClientModel;
 using Microsoft.Agents.AI;
 using Microsoft.AI.Foundry.Local;
-using Microsoft.Extensions.AI;
 using Microsoft.Extensions.Logging.Abstractions;
 using OpenAI;
 using OpenAI.Chat;
 using Shared;
 
-#region Start Foundry and download model if needed
-
 Utils.Init("Foundry.Local Sample");
 
-string modelAlias = "qwen2.5-coder-0.5b";
-Console.WriteLine($"Starting AI Model '{modelAlias}'. If not already started / cached this might take a while...");
+const string modelAlias = "qwen3-0.6b";
+const DeviceType deviceType = DeviceType.CPU;
+
+Utils.Green($"Starting AI Model '{modelAlias}'. If not already started / cached this might take a while...");
 string foundryDataDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".foundry");
 
-Console.WriteLine($"Using Foundry data directory: {foundryDataDir} (Try removing this folder if code hangs, or cause Exceptions");
-Console.WriteLine("Initializing Foundry Local SDK...");
+Utils.Gray($"- Using Foundry data directory: {foundryDataDir} (Try removing this folder if code hangs, or cause Exceptions");
+Utils.Gray("- Initializing Foundry Local SDK...");
 await FoundryLocalManager.CreateAsync(
     new Configuration
     {
@@ -29,48 +28,65 @@ await FoundryLocalManager.CreateAsync(
     },
     NullLogger.Instance);
 
-FoundryLocalManager manager = FoundryLocalManager.Instance;
+FoundryLocalManager foundryLocal = FoundryLocalManager.Instance;
 
-ICatalog catalog = await manager.GetCatalogAsync();
-IModel modelFamily = await catalog.GetModelAsync(modelAlias) ?? throw new InvalidOperationException($"Model '{modelAlias}' not found.");
-
-
-IModel model = modelFamily.Variants.First(v => v.Info.Runtime?.DeviceType == DeviceType.GPU); 
-Console.WriteLine($"Selected model variant: {model.Id}");
-
-bool isModelCached = await model.IsCachedAsync();
-if (!isModelCached)
+Utils.Gray("- Getting Catalog and Models...");
+ICatalog catalog = await foundryLocal.GetCatalogAsync();
+List<IModel> models = await catalog.ListModelsAsync();
+Utils.Gray("- Available Models");
+foreach (IModel availableModels in models)
 {
-    Console.WriteLine("Model not yet cached yet. Downloading...");
+    Utils.Gray($"-- {availableModels.Alias}");
+}
+
+IModel modelFamily = await catalog.GetModelAsync(modelAlias) ?? throw new InvalidOperationException($"Model '{modelAlias}' not found.");
+IModel model = modelFamily.Variants.First(v => v.Info.Runtime?.DeviceType == deviceType);
+Utils.Gray($"- Selected model variant: {model.Id}");
+
+if (!await model.IsCachedAsync())
+{
+    Utils.Gray("- Model not yet cached yet. Downloading...");
     await model.DownloadAsync();
 }
 
-Console.WriteLine("Loading model into memory...");
+Utils.Gray("- Loading model into memory...");
 await model.LoadAsync();
 
-Console.WriteLine("Starting local OpenAI-compatible web service...");
-await manager.StartWebServiceAsync();
+Utils.Gray("Starting local OpenAI-compatible web service...");
+await foundryLocal.StartWebServiceAsync();
 
-#endregion
+Utils.Separator();
 
-string serviceUrl = manager.Urls?.FirstOrDefault()
-    ?? throw new InvalidOperationException("Foundry Local did not report a web service URL.");
-Uri endpoint = new($"{serviceUrl}/v1");
-OpenAIClient client = new(new ApiKeyCredential("NO_API_KEY"), new OpenAIClientOptions
+Utils.Green("Creating OpenAI Compatible Client and Agent");
+try
 {
-    Endpoint = endpoint
-});
-ChatClientAgent agent = client.GetChatClient(model.Id).AsAIAgent();
+    string serviceUrl = foundryLocal.Urls!.First();
+    OpenAIClient client = new(new ApiKeyCredential("NO_API_KEY"), new OpenAIClientOptions
+    {
+        Endpoint = new Uri($"{serviceUrl}/v1")
+    });
+    ChatClientAgent agent = client.GetChatClient(model.Id).AsAIAgent();
 
-AgentResponse response = await agent.RunAsync("What is the Capital of Sweden?");
-Console.WriteLine(response);
+    //Normal
+    const string question1 = "What is the Capital of Sweden?";
+    Utils.Yellow($"Q: {question1}");
+    AgentResponse response = await agent.RunAsync(question1);
+    Console.WriteLine(response);
 
-Console.WriteLine("---");
+    Utils.Separator();
 
-await foreach (AgentResponseUpdate update in agent.RunStreamingAsync("How to make soup?"))
+    //Streaming
+    const string question2 = "How to make tomato soup?";
+    Utils.Yellow($"Q: {question2}");
+    await foreach (AgentResponseUpdate update in agent.RunStreamingAsync(question2))
+    {
+        Console.Write(update);
+    }
+}
+finally
 {
-    Console.Write(update);
+    await foundryLocal.StopWebServiceAsync();
+    await model.UnloadAsync();
 }
 
-await manager.StopWebServiceAsync();
-await model.UnloadAsync();
+Utils.Green("-- Done --");
