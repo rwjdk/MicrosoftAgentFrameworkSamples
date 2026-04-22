7db98e21b4dcf176153e56b81054719bd01dc754
Rasmus Wulff Jensen <rasmus.wulff.jensen@gmail.com>
2026-04-22 07:27:28 +0200

    WIP

---
+++ src/ZeroToFirstAgent.Cerebras/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Steps:
+ * 1: Get a Cerebras API Key (https://cloud.cerebras.ai/)
+ * 2: Add Nuget Package (Microsoft.Agents.AI.OpenAI)
+ * 3: Create an OpenAIClient with endpoint https://api.cerebras.ai/v1
+ * 4: Get a ChatClient and Create an AI Agent from it
+ * 5: Call RunAsync or RunStreamingAsync
+ */
+
+using System.ClientModel;
+using Microsoft.Agents.AI;
+using OpenAI;
+using OpenAI.Chat;
+
+const string apiKey = "<ApiKey>";
+const string model = "qwen-3-235b-a22b-instruct-2507";
+
+OpenAIClient client = new(new ApiKeyCredential(apiKey), new OpenAIClientOptions
+{
+    Endpoint = new Uri("https://api.cerebras.ai/v1")
+});
+ChatClientAgent agent = client.GetChatClient(model).AsAIAgent();
+
+AgentResponse response = await agent.RunAsync("What is the Capital of Finland?");
+Console.WriteLine(response);
+
+Console.WriteLine("---");
+
+await foreach (AgentResponseUpdate update in agent.RunStreamingAsync("How to make soup?"))
+{
+    Console.Write(update);
+}
---
+++ src/ZeroToFirstAgent.Cohere/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Steps:
+ * 1: Get a Cohere API Key (https://cloud.cerebras.ai/)
+ * 2: Add Nuget Package (Microsoft.Agents.AI.OpenAI)
+ * 3: Create an OpenAIClient with endpoint https://api.cerebras.ai/v1
+ * 4: Get a ChatClient and Create an AI Agent from it
+ * 5: Call RunAsync or RunStreamingAsync
+ */
+
+using System.ClientModel;
+using Microsoft.Agents.AI;
+using OpenAI;
+using OpenAI.Chat;
+
+const string apiKey = "<API Key>";
+const string model = "command-a-03-2025";
+
+OpenAIClient client = new(new ApiKeyCredential(apiKey), new OpenAIClientOptions
+{
+    Endpoint = new Uri("https://api.cohere.ai/compatibility/v1")
+});
+ChatClientAgent agent = client.GetChatClient(model).AsAIAgent();
+
+AgentResponse response = await agent.RunAsync("What is the Capital of Finland?");
+Console.WriteLine(response);
+
+Console.WriteLine("---");
+
+await foreach (AgentResponseUpdate update in agent.RunStreamingAsync("How to make soup?"))
+{
+    Console.Write(update);
+}
