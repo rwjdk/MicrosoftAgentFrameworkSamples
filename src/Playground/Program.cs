1acb40865d13f925cd21ff56d9080c72d7dbf33c
Rasmus Wulff Jensen <rasmus.wulff.jensen@gmail.com>
2026-06-23 23:41:31 +0200

    WIP

--- src/Playground/Program.cs
+++ src/Playground/Program.cs
@@ -1,46 +1,60 @@
 //WARNING: This is a playground area for the creator of the Repo to test and tinker. Nothing in this project is as such educational and might not even execute properly
 #pragma warning disable OPENAI002
+using Azure;
 using Azure.AI.OpenAI;
-using Azure.AI.Projects;
-using Azure.AI.Projects.Agents;
-using Azure.Identity;
 using Microsoft.Agents.AI;
-using Microsoft.Agents.AI.Hyperlight;
 using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Options;
 using OpenAI.Chat;
 using OpenAI.Responses;
+using Playground.Tools;
 using Shared;
-using System.ClientModel;
-using Microsoft.Agents.AI.Foundry;
+#pragma warning disable OPENAI001
+#pragma warning disable MEAI001
 
 Utils.Init("Playground");
 
 Secrets secrets = SecretsManager.GetSecrets();
 
-#pragma warning disable OPENAI001
-AIProjectClient client = new AIProjectClient(new Uri(secrets.AzureAiFoundryAgentEndpoint), new AzureCliCredential());
+AgentBuilderTool agentBuilderTool = new AgentBuilderTool();
 
-string model = "gpt-5.4-mini";
-string myAgentName = "myAgent";
+AzureOpenAIClient client = ClientHelper.GetAzureOpenAIClient();
 
-AIFunction tool = AIFunctionFactory.Create(GetWeather, "get_weather");
-client.AgentAdministrationClient.CreateAgentVersion(myAgentName, new ProjectsAgentVersionCreationOptions(
-    new DeclarativeAgentDefinition(model)
-    {
-        Instructions = "You are a nice AI",
-        Tools = { tool.AsOpenAIResponseTool() },
-        ReasoningOptions = new ResponseReasoningOptions
-        {
-            ReasoningEffortLevel = new ResponseReasoningEffortLevel("low")
-        }
-    }));
+AIAgent agent = client
+    .GetResponsesClient()
+    .AsAIAgent(
+        model: "gpt-5"
+        //tools: [AIFunctionFactory.Create(agentBuilderTool.RunSubAgent, "call_sub_agent")]
+        );
 
-FoundryAgent agent = client.AsAIAgent(myAgentName, tools: [tool]);
+AgentSession session = await agent.CreateSessionAsync();
 
-AgentResponse response = await agent.RunAsync("What is the weather like in Paris?");
-Console.WriteLine(response);
-
-static string GetWeather(string city)
+while (true)
 {
-    return "It is Sunny and 19 Degrees";
+    Console.Write("> ");
+    string? input = Console.ReadLine();
+    if (!string.IsNullOrWhiteSpace(input))
+    {
+        ChatClientAgentRunOptions options = new()
+        {
+            AllowBackgroundResponses = true,
+            ChatOptions = new ChatOptions
+            {
+            }
+        };
+        AgentResponse response = await agent.RunAsync(input, session, options: options);
+        int counter = 0;
+        while (response.ContinuationToken is not null)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(2));
+            counter++;
+            Utils.Gray($"- Waited: {(counter * 2)} seconds...");
+            options.ContinuationToken = response.ContinuationToken;
+            response = await agent.RunAsync(session, options);
+        }
+        Console.WriteLine(response);
+    }
+
+    Utils.Separator();
+
 }
---
+++ src/Playground/Tools/AgentBuilderTool.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using Azure.AI.OpenAI;
+using Microsoft.Agents.AI;
+using OpenAI.Chat;
+using Shared;
+
+namespace Playground.Tools;
+
+public class AgentBuilderTool
+{
+    public async Task<string> RunSubAgent([Description("Give the agent a funny name")]string agentName, string systemInstructions, string prompt)
+    {
+        Utils.Green($"Sub-agent '{agentName}': Instructions: '{systemInstructions}' - Prompt: '{prompt}'");
+
+        AzureOpenAIClient client = ClientHelper.GetAzureOpenAIClient();
+        ChatClientAgent agent = client.GetChatClient("gpt-4.1-mini").AsAIAgent(instructions: systemInstructions);
+        return (await agent.RunAsync(prompt)).ToString();
+    }
+}
